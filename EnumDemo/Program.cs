70ad784fcb27013d3a72aa43845722ecf3b4cbb0
Raber <Raber@DESKTOP-OFGAFQV>
2023-09-11 10:28:08 +0200

    version 3

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace EnumDemo
+﻿using System.Threading.Channels;
+
+namespace EnumDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, !");
-            Console.WriteLine("Detta är version 2");
-        }
+            Console.WriteLine("Detta är version 3");
+        }   
     }
 }
